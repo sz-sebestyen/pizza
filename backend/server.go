d6d2877d969abc58c65c10970068b5ec08e2678f
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-05-06 15:17:14 +0200

    Server menu from json file

---
+++ backend/server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("./")))
+
+	http.HandleFunc("/menu", menuHandler)
+
+	log.Println("Listening on :3000...")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func menuHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/menu.json", 304)
+}
