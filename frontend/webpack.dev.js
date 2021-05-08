35924774babd4326d89450347d2b5334674efe8c
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-05-08 10:06:01 +0200

    Add tailwindcss

--- frontend/webpack.dev.js
+++ frontend/webpack.dev.js
@@ -1,10 +1,21 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const path = require("path");
 
 module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
   devServer: {
     contentBase: "./dist",
+    watchContentBase: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
   },
 });
--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,6 +1,24 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
 module.exports = merge(common, {
   mode: "production",
+  plugins: [MiniCssExtractPlugin],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin()],
+  },
 });
