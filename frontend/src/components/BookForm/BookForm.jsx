257426861b8c629ae9521982f39f87dd21502b41
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-05-09 00:41:51 +0200

    Add inputs to the booking form

--- frontend/src/components/BookForm/BookForm.jsx
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -11,6 +11,21 @@ function BookForm({ setIsBooking }) {
       <div>
         <CloseButton {...{ setIsBooking }} />
 
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" />
+
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+
+        <label htmlFor="date">Date</label>
+        <input type="date" name="date" id="date" />
+
+        <label htmlFor="time">Time</label>
+        <input type="time" name="time" id="time" />
+
+        <label htmlFor="seats">Number of seats</label>
+        <input type="number" name="seats" id="seats" />
+
         <Submit />
       </div>
     </div>
