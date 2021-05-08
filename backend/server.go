49761df57f05f34bc3db14de0b3c35836f113200
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-05-08 12:00:03 +0200

    Set port from env

--- backend/server.go
+++ backend/server.go
@@ -3,18 +3,34 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
 	http.Handle("/", http.FileServer(http.Dir("./")))
 
 	http.HandleFunc("/menu", menuHandler)
 
-	log.Println("Listening on :3000...")
-	err := http.ListenAndServe(":3000", nil)
+	waitForAction(getPort())
+}
+
+func waitForAction(port string) {
+	log.Println("Listening on :" + port + "...")
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getPort() string {
+	port, portIsSet := os.LookupEnv("PORT")
+
+	if !portIsSet {
+		port = "3000"
+	}
+
+	return port
 }
 
 func menuHandler(w http.ResponseWriter, r *http.Request) {
